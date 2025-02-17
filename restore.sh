4cb8c17e52005d2c6ee1bfd70ee428c17ed0572c
JuanesCano <133620876+JuanesCano@users.noreply.github.com>
2025-02-17 01:20:55 -0500

    Delete restore.sh

--- restore.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Restaurando la base de datos desde el backup..."
-
-# Restaurar la base de datos
-mongorestore --uri="mongodb://localhost:27017" ./backup/test
-
-echo "Restauración completada."
